5e7bb8f9356ec015a36cb9cd898a6ab0fa172c0b
Alexander Kleinschmidt <alexander.kleinschmidt@smail.fh-koeln.de>
2016-11-22 00:30:38 +0100

    create new c funktion with libnfc as base

---
+++ bin/nfc-mifare-wait.c
@@ -0,0 +1,51 @@
+// To compile:
+// $ gcc -o nfc-mifare-wait nfc-mifare-wait.c -lnfc
+
+#include <stdlib.h>
+#include <nfc/nfc.h>
+
+int main(int argc, const char *argv[])
+{
+  nfc_device *pnd;
+  nfc_target nt;
+
+  // Allocate only a pointer to nfc_context
+  nfc_context *context;
+
+  // Initialize libnfc and set the nfc_context
+  nfc_init(&context);
+  if (context == NULL) {
+    printf("Unable to init libnfc (malloc)\n");
+    exit(EXIT_FAILURE);
+  }
+
+
+  pnd = nfc_open(context, NULL);
+
+  if (pnd == NULL) {
+    printf("ERROR: %s\n", "Unable to open NFC device.");
+    exit(EXIT_FAILURE);
+  }
+  // Set opened NFC device to initiator mode
+  if (nfc_initiator_init(pnd) < 0) {
+    nfc_perror(pnd, "nfc_initiator_init");
+    exit(EXIT_FAILURE);
+  }
+
+  printf("NFC reader: %s opened\n", nfc_device_get_name(pnd));
+
+  // Poll for a ISO14443A (MIFARE) tag
+  const nfc_modulation nmMifare = {
+    .nmt = NMT_ISO14443A,
+    .nbr = NBR_106,
+  };
+  if (nfc_initiator_select_passive_target(pnd, nmMifare, NULL, 0, &nt) > 0) {
+    printf("NFC ISO14443A tag was found:\n");
+
+  }
+  // Close NFC device
+  nfc_close(pnd);
+  // Release the context
+  nfc_exit(context);
+  exit(EXIT_SUCCESS);
+}
